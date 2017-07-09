f48731ec508ee17c2cd34d36b42632220521e1a8
ChangChao-Tang <mrgmp2004@hotmail.com>
2017-07-09 15:35:43 +0800

    add appMediator

---
+++ PortablePhotoStudio360/AppMediator.swift
@@ -0,0 +1,23 @@
+//
+//  AppMediator.swift
+//  PortablePhotoStudio360
+//
+//  Created by ChangChao-Tang on 2017/7/9.
+//  Copyright © 2017年 ChangChao-Tang. All rights reserved.
+//
+
+import UIKit
+
+class AppMediator: NSObject {
+    
+    private let window:UIWindow
+    
+    init(withWindow window:UIWindow) {
+        self.window = window
+    }
+    
+    func start() {
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+    }
+}
