467ae73a2add7d55433d196194fa571dd6521147
ChangChao-Tang <mrgmp2004@hotmail.com>
2017-07-11 19:46:09 +0800

    add test ui

--- PortablePhotoStudio360/AppMediator.swift
+++ PortablePhotoStudio360/AppMediator.swift
@@ -17,7 +17,15 @@ class AppMediator: NSObject {
     }
     
     func start() {
-        window.rootViewController = TestViewController()
+        let test = TestViewController()
+        test.mediator = self
+        window.rootViewController = test
         window.makeKeyAndVisible()
+    }
+    
+    func showAlert(message:String, onController controller:UIViewController) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        controller.present(alert, animated: true, completion: nil)
     }
 }
--- PortablePhotoStudio360/TestViewController.swift
+++ PortablePhotoStudio360/TestViewController.swift
@@ -7,14 +7,90 @@
 //
 
 import UIKit
+import PinLayout
+import CoreBluetooth
 
 class TestViewController: UIViewController {
-
+    
+    weak var mediator:AppMediator?
+    let PeripheralListCellReuseId = "PeripheralListCellReuseId"
+    let PeriphearlListRowHeight:CGFloat = 44.0
+    let PeriphearlListMaxVisibleRows = 3
+    var periphearlListHeight:CGFloat {
+        get{
+            let maxHeight = CGFloat(PeriphearlListMaxVisibleRows) * PeriphearlListRowHeight
+            let height = CGFloat(peripherals.count) * PeriphearlListRowHeight
+            return height < maxHeight ? height : maxHeight
+        }
+    }
+    var peripherals = [AvailablePeripheralData]()
+    let scanButton = UIButton()
+    let peripheralList = UITableView()
+    var bluetoothService:BluetoothCentralService?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.white
+        setupUI()
+        bluetoothService = BluetoothCentralService(withServices: []) {[unowned self](state) in
+            switch state {
+            case .poweredOff:
+                self.mediator?.showAlert(message: "Bluetooth power off", onController: self)
+                break
+            case .poweredOn:
+                self.scanButton.isEnabled = true
+                self.scanButton.alpha = 1
+                break
+            case .unsupported:
+                self.mediator?.showAlert(message: "Unspported device", onController: self)
+                break
+            case .unauthorized:
+                self.mediator?.showAlert(message: "Need auth", onController: self)
+                break
+            default:
+                //unknown restting ignore..
+                break
+            }
+        }
     }
     
+    private func setupUI (){
+        view.backgroundColor = UIColor.white
+        view.addSubview(scanButton)
+        view.addSubview(peripheralList)
+        scanButton.setTitle("Scan", for: .normal)
+        scanButton.setTitleColor(UIColor.blue, for: .normal)
+        scanButton.isEnabled = false
+        scanButton.alpha = 0.25
+        scanButton.addTarget(self, action: #selector(onScan), for: .touchUpInside)
+        peripheralList.register(UITableViewCell.self, forCellReuseIdentifier: PeripheralListCellReuseId)
+        peripheralList.dataSource = self
+    }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let status = UIApplication.shared.statusBarFrame
+        scanButton.pin.top(status.height + 10).right(10).width(45).height(30)
+        peripheralList.pin.left(10).right(10).bottom(10).below(of: scanButton).marginTop(10)
+    }
+    
+    func onScan() {
+        bluetoothService?.startScan(discoverHandler: { (data) in
+            self.peripherals = data
+            self.peripheralList.reloadData()
+        })
+    }
+}
+
+extension TestViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return peripherals.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: PeripheralListCellReuseId, for: indexPath)
+        let inform = peripherals[indexPath.row]
+        cell.textLabel?.text = inform.peripheral.identifier.uuidString
+        return cell
+    }
 }
 
